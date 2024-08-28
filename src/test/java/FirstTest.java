08d0ab19082bddc8a2273f65a3b511b7438a77c8
Nirmal Baral <1761199+ncbaral@users.noreply.github.com>
2024-08-28 17:01:23 +0100

    JAVA Selenium setup with Gradle

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        driver = getWebDriver();
+    }
+
+    @Test
+    public void firstTest() {
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000000));
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        String title = driver.getTitle();
+        assertEquals("Web form", title);
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        String textMessage = message.getText();
+        assertEquals("Received!", textMessage);
+    }
+
+    private static WebDriver getWebDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-search-engine-choice-screen");
+        options.setExperimentalOption("excludeSwitches", List.of("enable-automation"));
+        options.addArguments("--disable-search-engine-choice-screen");
+        options.addArguments("--disable-features=OptimizationGuideModelDownloading,OptimizationHintsFetching,OptimizationTargetPrediction,OptimizationHints");
+
+        return new ChromeDriver(options);
+    }
+
+    @AfterEach
+    public void teardown() {
+        driver.quit();
+    }
+}
